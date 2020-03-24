9c8f834c6658dd46ddbbb928a295200142954e10
KlowiChloe <chloe.ryckx@telenet.be>
2020-03-24 21:53:52 +0100

    user, db, settings en register aangemaakt
    settings: geen fouten
    db: geen fouten
    register: endswith nakijken
    user: endswith functie in email fout
    andere teamleden zouden code eens moeten testen op database -> return leeg ipv data

---
+++ classes/User.php
@@ -0,0 +1,161 @@
+<?php
+
+include_once(__DIR__ . "/Db.php");
+
+
+class User
+{
+    private $email;
+    private $firstname;
+    private $lastname;
+    private $password;
+
+
+    /**
+     * Get the value of email
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */
+    public function setEmail($email)
+    { if( empty($email)){
+        throw new Exception("email cannot be empty");
+    }
+
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of firstname
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set the value of firstname
+     *
+     * @return  self
+     */
+    public function setFirstname($firstname)
+    {
+        if (empty($firstname)) {
+            throw new Exception("Firstname cannot be empty");
+        }
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of lastname
+     */
+    public function getLastname()
+    { 
+        return $this->lastname;
+    }
+
+    /**
+     * Set the value of lastname
+     *
+     * @return  self
+     */
+    public function setLastname($lastname)
+    {if(empty($lastname)){
+        throw new Exception("Lastname cannot be empty");
+    }
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     */
+    public function getPassword()
+    { 
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @return  self
+     */
+    public function setPassword($password)
+    {   if(empty($password)){
+        //secure those passwords
+        //$salt = "djghdiguhosifj";
+        throw new Exception("password cannot be empty!");
+    }
+
+        $password = password_hash($password, PASSWORD_DEFAULT, ['cost' => 14]); // niet goed --> md5($password.$salt); //te snel
+       $this->password = $password;
+
+       return $this;
+      
+    }
+    public function save()
+    {
+        try {
+        //conn
+        $conn = Db::getConnection();
+        //insert query
+        $statement = $conn->prepare("insert into users(firstname,lastname,email,password) values (:firstname, :lastname, :email, :password)");
+        $firstname = $this->getFirstName();
+        $lastname = $this->getLastName();
+        $email = $this->getEmail();
+        $password = $this->getPassword();
+
+
+        $statement->bindParam(":firstname", $firstname);
+        $statement->bindParam(":lastname", $lastname);
+        $statement->bindParam(":email", $email);
+        $statement->bindParam(":password", $password);
+
+        $result = $statement->execute();
+
+        return $result;
+
+        
+
+        //return result
+        } catch ( Throwable $t ) {
+            print "Error!: " . $t->getMessage() . "<br/>";
+            die();
+        }
+
+
+
+
+    }
+    
+
+function endsWith($email, $endString)
+{
+    $len = strlen($endString);
+   return(substr($email,0,$len) === $endString);
+   
+   
+   
+   
+    if (endsWith("@student.thomasmore.be")) {
+        echo true;
+    }else{
+        throw new Exception("email has to end on @student.thomasmore.be");
+    }
+
+   
+}
+
+    }
--- register.php
+++ register.php
@@ -1,1 +1,92 @@
+<?php
 
+//Connectie klasses
+include_once(__DIR__ . "/classes/User.php");
+
+if (!empty($_POST)) {
+    try {
+        $user = new User();
+        $user->setEmail(htmlspecialchars($_POST['email']));
+        $user->setFirstName(htmlspecialchars($_POST['firstname']));
+        $user->setLastName(htmlspecialchars($_POST['lastname']));
+        $user->setPassword(htmlspecialchars($_POST['password']));
+
+        $user->save();
+       // $success = "User saved!";
+
+        $user->endsWith();
+
+    } 
+    catch (Throwable $th) {
+        $error = $th->getMessage();
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Register</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container--login">
+        <div class="wrap">
+
+            <div class="form-group">
+
+
+
+                <?php if (isset($error)) : ?>
+                    <div class="error" style="color:red;">
+                        <?php echo $error; ?>
+                    </div>
+                <?php endif; ?>
+
+              <!--  <?php if (isset($success)) : ?>
+                    <div class="success" style="color:green;">
+                        <?php echo $success; ?>
+                    </div>
+                <?php endif; ?> !-->
+
+                <form action="" method="post">
+                    <h2 form__title>Create an account</h2>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="text" class="form-control" id="email" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="firstname">Firstname</label>
+                        <input type="text" class="form-control" id="firstname" name="firstname">
+                    </div>
+                    <div class="form-group">
+                        <label for="lastname">Lastname</label>
+                        <input type="text" class="form-control" id="lastname" name="lastname">
+                    </div>
+
+
+
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" name="password">
+                    </div>
+
+
+
+                    <div class="form__btn">
+                        <input type="submit" class="btn btn-primary" name="submit" value="Sign me up!">
+                    </div>
+                </form>
+                <div class="link">
+                    <p> <a href="login.php"> Back to login</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
