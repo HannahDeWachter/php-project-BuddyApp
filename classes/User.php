b12326d4e701576ea881cdf9a0715d6d7fcb551d
HannahDeWachter <r0738008@student.thomasmore.be>
2020-03-25 14:18:46 +0100

    updateUser function

--- classes/User.php
+++ classes/User.php
@@ -1,9 +1,11 @@
 <?php
+include_once(__DIR__ . "/Db.php");
+
 class User
 {
-    private $id;
-    private $firstname;
-    private $lastname;
+    protected $id;
+    protected $firstname;
+    protected $lastname;
     private $email;
     private $password;
 
@@ -12,4 +14,53 @@ class User
     private $hobbies;
     private $specialization;
     private $travel;
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+
+    public function updateUser()
+    {
+        $conn = Db::getConnection();
+        $statement = $conn->prepare("UPDATE `users` SET `location`=:location,`music`=:music,`hobbies`=:hobies,`specialization`=:specialization, `travel`=:travel where user_id = :userid");
+
+        $userid = $this->getId();
+        $statement->bindParam(":userid", $userid);
+        $statement->bindParam(":location", $location);
+        $statement->bindParam(":music", $music);
+        $statement->bindParam(":hobbiees", $hobbies);
+        $statement->bindParam(":specialization", $specialization);
+        $statement->bindParam(":travel", $travel);
+        $statement->execute();
+
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
 }
