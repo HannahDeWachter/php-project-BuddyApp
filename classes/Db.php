df8afecc7e6db70551e93af49a547918ffe4020a
HannahDeWachter <r0738008@student.thomasmore.be>
2020-03-25 17:44:22 +0100

    fix the database connection

--- classes/Db.php
+++ classes/Db.php
@@ -8,7 +8,7 @@ class Db
         include_once(__DIR__ . "/../settings/Settings.php");
 
         if (self::$conn === null) {
-            self::$conn = new PDO('mysql:host=' . SETTINGS['db']['host'] . ';dbname=' . SETTINGS['db']['name'],  SETTINGS['db']['user'], SETTINGS['db']['password']);
+            self::$conn = new PDO('mysql:host=' . SETTINGS['db']['host'] . ';dbname=' . SETTINGS['db']['dbname'],  SETTINGS['db']['user'], SETTINGS['db']['password']);
             return self::$conn;
         } else {
             return self::$conn;
