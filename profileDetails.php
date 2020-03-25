a71d399643dffd007747cc3ec1016924d7a53282
HannahDeWachter <r0738008@student.thomasmore.be>
2020-03-25 14:17:46 +0100

    show form values

--- profileDetails.php
+++ profileDetails.php
@@ -1,13 +1,33 @@
 <?php
 
 include_once(__DIR__ . "/classes/User.php");
-include_once(__DIR__ . "/includes/header.inc.php");
+include_once(__DIR__ . "/classes/Db.php");
 
-if (isset($_POST)) {
-    if (isset($_POST['music'])) {
-        $music = implode(',', $_POST['music']);
-        echo $music;
+
+if (!empty($_POST)) {
+    $location = $_POST['location'];
+    $specialization = $_POST['specialization'];
+
+    if (!empty($_POST['travel'])) {
+        $travel = $_POST['travel'];
+        $travel = implode(',', $travel);
+        echo "$travel<br />\n";
     }
+    if (!empty($_POST['music'])) {
+        $music = $_POST['music'];
+        $music = implode(',', $music);
+        echo "$music<br />\n";
+    }
+    if (!empty($_POST['hobbies'])) {
+        $hobbies = $_POST['hobbies'];
+        $hobbies = implode(',', $hobbies);
+        echo "$hobbies<br />\n";
+    }
+
+    echo "$specialization<br />\n";
+    echo "$location<br />\n";
+
+    // User::updateUser();
 }
 
 ?>
@@ -22,44 +42,58 @@ if (isset($_POST)) {
 </head>
 
 <body>
-    <form action="POST">
-        <label for="location">Where do you live?</label><br>
+    <?php include_once(__DIR__ . "/includes/header.inc.php"); ?>
+
+
+    <form action="" method="post">
+        <label for="location">In what city do you live?</label><br>
         <input type="text" name="location" id="location">
         <br><br>
         <label for="specialization">Design or Development?</label><br>
         <select name="specialization" id="specialization">
-            <option value="design">Design</option>
-            <option value="dev">Development</option>
+            <option <?php if (isset($specialization) && $specialization === "design") {
+                        echo "selected";
+                    } ?> value="design">Design</option>
+            <option <?php if (isset($specialization) && $specialization === "design") {
+                        echo "selected";
+                    } ?> value="dev">Development</option>
+            <option <?php if (isset($specialization) && $specialization === "design") {
+                        echo "selected";
+                    } ?> value="both">Both</option>
         </select>
         <br><br>
         <label for="music">Which music do you like?</label><br>
-        <input type="checkbox" class="checkbox-inline" name="music" value="pop">Pop
+        <input type="checkbox" class="checkbox-inline" id="music" name="music[]" value="pop">Pop
         <br>
-        <input type="checkbox" class="checkbox-inline" name="music" value="rock">Rock
+        <input type="checkbox" class="checkbox-inline" id="music" name="music[]" value="rock">Rock
         <br>
-        <input type="checkbox" class="checkbox-inline" name="music" value="rnb">RnB
+        <input type="checkbox" class="checkbox-inline" id="music" name="music[]" value="rnb">RnB
         <br>
-        <input type="checkbox" class="checkbox-inline" name="music" value="techno">Techno
+        <input type="checkbox" class="checkbox-inline" id="music" name="music[]" value="techno">Techno
         <br>
-        <input type="checkbox" class="checkbox-inline" name="music" value="kpop">K-pop
+        <input type="checkbox" class="checkbox-inline" id="music" name="music[]" value="kpop">K-pop
+        <br>
+        <input type="checkbox" class="checkbox-inline" id="music" name="music[]" value="other">Other
         <br><br>
         <label for="hobbies">What do you like to do?</label><br>
-        <input type="checkbox" class="checkbox-inline" name="hobbies" value="sport">Sport
+        <input type="checkbox" class="checkbox-inline" id="hobbies" name="hobbies[]" value="sport">Sport
         <br>
-        <input type="checkbox" class="checkbox-inline" name="hobbies" value="instrument">Play an instrument
+        <input type="checkbox" class="checkbox-inline" id="hobbies" name="hobbies[]" value="instrument">Play an instrument
         <br>
-        <input type="checkbox" class="checkbox-inline" name="hobbies" value="sing">Sing
+        <input type="checkbox" class="checkbox-inline" id="hobbies" name="hobbies[]" value="sing">Sing
+        <br>
+        <input type="checkbox" class="checkbox-inline" id="hobbies" name="hobbies[]" value="other">Other
         <br><br>
-        <label for="travel">Where have you traveled?</label><br>
-        <input type="checkbox" class="checkbox-inline" name="travel" value="africa">Africa
+        <label for="travel">Where have you travelled?</label><br>
+        <input type="checkbox" class="checkbox-inline" id="travel" name="travel[]" value="africa">Africa
         <br>
-        <input type="checkbox" class="checkbox-inline" name="travel" value="america">America
+        <input type="checkbox" class="checkbox-inline" id="travel" name="travel[]" value="america">America
         <br>
-        <input type="checkbox" class="checkbox-inline" name="travel" value="asia">Asia
+        <input type="checkbox" class="checkbox-inline" id="travel" name="travel[]" value="asia">Asia
         <br>
-        <input type="checkbox" class="checkbox-inline" name="travel" value="europe">Europe
+        <input type="checkbox" class="checkbox-inline" id="travel" name="travel[]" value="europe">Europe
         <br>
-        <input type="checkbox" class="checkbox-inline" name="travel" value="oceania">Oceania
+        <input type="checkbox" class="checkbox-inline" id="travel" name="travel[]" value="oceania">Oceania
         <br><br>
         <input type="submit" value="Save">
     </form>
