c6e73e817d4b244492da585623513585e4ec383f
HannahDeWachter <r0738008@student.thomasmore.be>
2020-03-25 14:18:29 +0100

    set not completed profile message

--- profile.php
+++ profile.php
@@ -1,7 +1,13 @@
 <?php
 
 include_once(__DIR__ . "/classes/User.php");
-include_once(__DIR__ . "/includes/header.inc.php");
+include_once(__DIR__ . "/classes/Db.php");
+
+// checken of velden (location, music, travel, specialization, hobbies) allemaal zijn ingevuld
+if (empty($location) || empty($music) || empty($travel) || empty($specialization) || empty($hobbies)) {
+    // als niet iets ingevuld -> $message = "You have not completed your profile."
+    $message = "You have not completed your profile.";
+}
 
 ?>
 <!DOCTYPE html>
@@ -14,8 +20,13 @@ include_once(__DIR__ . "/includes/header.inc.php");
 </head>
 
 <body>
+    <?php include_once(__DIR__ . "/includes/header.inc.php"); ?>
+
     <img src="" alt="" id="profilePic">
     <strong id="name"></strong>
+    <!-- <php if(isset($message)): ?>
+        <div class="alert"><php echo $message ?> Click <a href="">here</a> to complete your profile.</div>
+     <php endif; ?> -->
 </body>
 
 </html>
