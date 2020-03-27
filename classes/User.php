372291365c9e75d331f6b0d4b88763e14e43f6ad
HannahDeWachter <r0738008@student.thomasmore.be>
2020-03-27 17:28:58 +0100

    register and login works

--- classes/User.php
+++ classes/User.php
@@ -2,7 +2,6 @@
 
 include_once(__DIR__ . "/Db.php");
 
-
 class User
 {
     private $email;
@@ -120,10 +119,10 @@ class User
         $password = $this->getPassword();
 
 
-        $statement->bindParam(":firstname", $firstname);
-        $statement->bindParam(":lastname", $lastname);
-        $statement->bindParam(":email", $email);
-        $statement->bindParam(":password", $password);
+        $statement->bindValue(":firstname", $firstname);
+        $statement->bindValue(":lastname", $lastname);
+        $statement->bindValue(":email", $email);
+        $statement->bindValue(":password", $password);
 
         $result = $statement->execute();
         echo "ik ben hier aan het saven";
@@ -157,111 +156,45 @@ class User
         }
     }
 
-
     public static function canLogin($email, $password)
-        {
-            //db connectie
-            $conn = new PDO('mysql:host=localhost;dbname=login', "root", "");
+    {
+        //db connectie
+        $conn = Db::getConnection();
 
-            //email zoeken in db
-            $statement = $conn->prepare('select * from users where email = :email');
-            $statement->bindParam(':email', $email);
-            $statement->execute();
-            $user = $statement->fetch(PDO::FETCH_ASSOC);
+        //email zoeken in db
+        $statement = $conn->prepare('select * from users where email = :email');
+        $statement->bindParam(':email', $email);
+        $statement->execute();
+        $user = $statement->fetch(PDO::FETCH_ASSOC);
 
-            //passwoorden komen overeen?
-            if (password_verify($password, $user['password'])) {
-                //ja -> naar index
-                //echo "joepie de poepie!!!!";
-                return $user;
-            } else {
-                //nee -> error
-                //echo "jammer joh";
-                return false;
-            }
-        }
-
-        public static function doLogin($user)
-        {
-            $_SESSION['email'] = $user['email'];
-            $_SESSION['user_id'] = $user['id'];
-            header('location: index.php');
-        }
-
-        public static function getUserId()
-        {
-            $email = $_SESSION['email'];
-            $conn = new PDO('mysql:host=localhost;dbname=login', "root", "");
-            $statement = $conn->prepare('select id from users where email = :email');
-            $statement->bindParam(':email', $email);
-            $statement->execute();
-            $user = $statement->fetch(PDO::FETCH_ASSOC);
-
-            return $user['id'];
-        }
-}
-    
-   
-
-        
-        
-        
-
-
-
-
-
-
-?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-<!---     public function save(){
-            //conn
-            $conn = new PDO('mysql:host=localhost;dbname=login', "root", "");
-            
-            $statement = $conn->prepare("insert into users(email, password) values (:email, :password)");
-
-            $email = $this->getEmail();
-            $password = $this->getPassword();
-
-
-            $statement->bindValue(":email", $email);
-            $statement->bindValue(":password", $password);
-
-            $result = $statement->execute();
-
-            return $result;
-
-        }
-
-        public function checkLogin(){
-                
+        //passwoorden komen overeen?
+        if (password_verify($password, $user['password'])) {
+            //ja -> naar index
+            //echo "joepie de poepie!!!!";
+            return $user;
+        } else {
+            //nee -> error
+            //echo "jammer joh";
+            return false;
         }
     }
 
+    public static function doLogin($user)
+    {
+        $_SESSION['email'] = $user['email'];
+        $_SESSION['user_id'] = $user['id'];
+        header('location: index.php');
+    }
 
+    public static function getUserId()
+    {
+        $email = $_SESSION['email'];
+        $conn = new PDO('mysql:host=localhost;dbname=login', "root", "");
+        $statement = $conn->prepare('select id from users where email = :email');
+        $statement->bindParam(':email', $email);
+        $statement->execute();
+        $user = $statement->fetch(PDO::FETCH_ASSOC);
+
+        return $user['id'];
+    }
+}
