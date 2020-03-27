0753363c3bdd81739116a53c685da36db9dec740
HannahDeWachter <r0738008@student.thomasmore.be>
2020-03-27 10:43:56 +0100

    message to complete profile

--- profile.php
+++ profile.php
@@ -5,8 +5,8 @@ include_once(__DIR__ . "/classes/Db.php");
 
 // checken of velden (location, music, travel, specialization, hobbies) allemaal zijn ingevuld
 if (empty($location) || empty($music) || empty($travel) || empty($specialization) || empty($hobbies)) {
-    // als niet iets ingevuld -> $message = "You have not completed your profile."
-    $message = "You have not completed your profile.";
+    // als niet iets ingevuld -> $message = "You have not completed your profile yet."
+    $message = "You have not completed your profile yet.";
 }
 
 ?>
@@ -16,6 +16,7 @@ if (empty($location) || empty($music) || empty($travel) || empty($specialization
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
     <title>Buddy Profile</title>
 </head>
 
@@ -24,9 +25,11 @@ if (empty($location) || empty($music) || empty($travel) || empty($specialization
 
     <img src="" alt="" id="profilePic">
     <strong id="name"></strong>
-    <!-- <php if(isset($message)): ?>
-        <div class="alert"><php echo $message ?> Click <a href="">here</a> to complete your profile.</div>
-     <php endif; ?> -->
+    <php if(isset($message)): ?>
+        <div class="alert-warning">
+            <p><?php echo $message ?> Click <a href="profileDetails.php">here</a> to complete your profile.</p>
+        </div>
+        <php endif; ?>
 </body>
 
 </html>
