6cba526143b0c19f2aa0732a59275e1e30b1d970
SimonHostyn <44255447+SimonHostyn@users.noreply.github.com>
2020-05-04 12:37:08 +0200

    update online style

--- login.php
+++ login.php
@@ -23,10 +23,11 @@ if (!empty($_POST)) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Login</title>
+  <link href="css/style.css" rel="stylesheet" />
+
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"> !-->
   <!-- CSS Files -->
-  <link href="css/style.css" rel="stylesheet" />
 </head>
 
 <body>
